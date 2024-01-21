b9ae4fe8e501b209d01347942051d1780bcab272
Kambar <kidkambar@gmail.com>
2024-01-21 11:25:55 +0500

    feat: add solution for 05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const distPath = path.join(__dirname, 'project-dist');
+const stylesPath = path.join(__dirname, 'styles');
+
+const bundle = fs.createWriteStream(path.join(distPath, 'bundle.css'));
+
+fs.readdir(stylesPath, (_, files) => {
+  for(let i = 0; i < files.length; i++) {
+    if(path.extname(files[i]) === '.css') {
+      console.log(files[i])
+      const rs = fs.createReadStream(path.join(stylesPath, files[i]), 'utf-8')
+      rs.on('data', (chunk) => bundle.write(chunk))
+    }
+  }
+})
